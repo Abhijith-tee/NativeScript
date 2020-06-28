e1e854b8016fddc811a4cb335e369e3348b1c8f3
Abhijith-tee <63539211+Abhijith-tee@users.noreply.github.com>
2020-06-28 12:23:58 +0530

    Image Resources

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -10,6 +10,9 @@ import { Page } from "ui/page";
 import { View } from "ui/core/view";
 import { SwipeGestureEventData, SwipeDirection } from "ui/gestures";
 import * as enums from "ui/enums";
+import * as app from "application";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+
 
 @Component({
   selector: 'app-home',
@@ -50,6 +53,11 @@ export class HomeComponent implements OnInit {
       .subscribe(leader => this.leader = leader,
         errmess => this.leaderErrMess = <any>errmess);
   }
+
+  onDrawerButtonTap(): void {
+    const sideDrawer = <RadSideDrawer>app.getRootView();
+    sideDrawer.showDrawer();
+}
 
   onSwipe(args: SwipeGestureEventData) {
 
